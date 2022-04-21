a0e05ba7df5d004267833142097ed65e3a3a7785
Revolution5 <damonboone99@gmail.com>
2022-04-21 16:49:22 -0700

    Form stuff

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let myLibrary = [];
 
-function Book(author, title, pages, read) {
-    this.author = author;
+function Book(title, author, pages, read) {
     this.title = title;
+    this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-function addBookToLibrary(author, title, pages, read) {
-    let newBook = new Book(author, title, pages, read);
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
@@ -18,13 +18,13 @@ function displayBooks() {
         let book = document.createElement("div");
         book.classList.add("book");
 
-        let authorDisplay = document.createElement("p");
-        authorDisplay.textContent = "Author: " + myLibrary[i].author;
-        book.appendChild(authorDisplay);
-
         let titleDisplay = document.createElement("p");
         titleDisplay.textContent = "Title: " + myLibrary[i].title;
         book.appendChild(titleDisplay);
+
+        let authorDisplay = document.createElement("p");
+        authorDisplay.textContent = "Author: " + myLibrary[i].author;
+        book.appendChild(authorDisplay);
 
         let pageDisplay = document.createElement("p");
         pageDisplay.textContent = "Page Count: " + myLibrary [i].pages;
@@ -34,25 +34,40 @@ function displayBooks() {
     }
 }
 
+function showForm() {
+    document.querySelector(".pop-up-form").style.display = "block";
+    document.querySelector("*").style.backgroundColor = "rgba(22,22,22,0.5)"
+}
+
+function closeForm() {
+    document.querySelector(".pop-up-form").style.display = "none";
+    document.querySelector("*").style.backgroundColor = "green";
+}
+
+let submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", function(e) {
+    let userAuthor = document.querySelector("#author");
+    let userTitle = document.querySelector("#title");
+    let userPages = document.querySelector("#pages");
+
+    addBookToLibrary(userTitle, userAuthor, userPages, true);
+    location.reload();
+})
+
+
+
+
+
 addBookToLibrary("ac", "b", 111, true);
-addBookToLibrary("a", "bc", 111, true);
-addBookToLibrary("a2", "bd", 111, true);
-addBookToLibrary("aee", "be", 111, true);
-addBookToLibrary("af", "b", 111, true);
 addBookToLibrary("ac", "b", 111, true);
-addBookToLibrary("a", "bc", 111, true);
-addBookToLibrary("a2", "bd", 111, true);
-addBookToLibrary("aee", "be", 111, true);
-addBookToLibrary("af", "b", 111, true);
 addBookToLibrary("ac", "b", 111, true);
-addBookToLibrary("a", "bc", 111, true);
-addBookToLibrary("a2", "bd", 111, true);
-addBookToLibrary("aee", "be", 111, true);
-addBookToLibrary("af", "b", 111, true);
 addBookToLibrary("ac", "b", 111, true);
-addBookToLibrary("a", "bc", 111, true);
-addBookToLibrary("a2", "bd", 111, true);
-addBookToLibrary("aee", "be", 111, true);
-addBookToLibrary("af", "b", 111, true);
+addBookToLibrary("ac", "b", 111, true);
+addBookToLibrary("ac", "b", 111, true);
+addBookToLibrary("ac", "b", 111, true);
+addBookToLibrary("ac", "b", 111, true);
+addBookToLibrary("ac", "b", 111, true);
+addBookToLibrary("ac", "b", 111, true);
+
 
 displayBooks();
